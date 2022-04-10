6bc244d95233495d946106a7a581762384d7438e
BEKMAL <102759163+BEKMAL@users.noreply.github.com>
2022-04-10 20:07:43 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char: ID byte(s)\n".size of(char));
+printf("Size of an int: ID byte(s)\n".size of(int));
+printf("Size of a long int: ID byte(s)\n".size of(long int));
+printf("Size of a long long long int: ID byte(s)\n".size of(long long long int));
+printf("Size of a float: ID byte(s)\n".size of(float));
+return (0);
+}

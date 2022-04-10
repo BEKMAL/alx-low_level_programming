de703ac30adc86510bd4a153814b8fdf47389450
BEKMAL <102759163+BEKMAL@users.noreply.github.com>
2022-04-10 21:43:58 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
-puts("and that piece of art is useful");
-puts("- Dora Korpar, 2015-10-19 ");
+puts("and that piece of art is useful'"' - Dora Korpar, 2015-10-19");
 return (1);
 }
